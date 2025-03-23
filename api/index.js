90f26b8dfe8a84c16ff4bb7b152cdbe4a7e2c4dc
CamouflageNetwork <notmrmacattax@gmail.com>
2025-03-22 22:42:02 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -14,9 +14,21 @@ export default async function handler(req, res) {
         });
 
         const contentType = response.headers.get("content-type");
-        if (contentType) res.setHeader("Content-Type", contentType);
 
+        if (contentType && contentType.includes("text/html")) {
+            let body = await response.text();
+            const erudaScript = `
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/eruda/2.4.1/eruda.min.js"></script>
+                <script>eruda.init();</script>
+            `;
+            body = body.replace("</body>", `${erudaScript}</body>`);
+            res.setHeader("Content-Type", "text/html");
+            return res.send(body);
+        }
+
+        res.setHeader("Content-Type", contentType);
         response.body.pipe(res);
+
     } catch (error) {
         res.status(500).json({ error: "Failed to fetch the requested URL." });
     }
