de94e08d2741fce3b66e1926be276c1a6b8d2c50
CamouflageNetwork <notmrmacattax@gmail.com>
2025-04-18 13:13:00 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -88,7 +88,7 @@ export default async function handler(req, res) {
                 <script>eruda.init();</script>
             </body>`);
             
-                data = data.replace(/url\((['"]?)\.\.\/([^)'"]+)\1\)/g, 'url($1..$2$1)');
+            data = data.replace(/url\(\.\.\/([^'")]+)\)/g, 'url(..$1)');
             
         }
         
