ad177b0b19f5f3727603a9a3a7c85abb8135ab61
CamouflageNetwork <notmrmacattax@gmail.com>
2025-04-22 19:03:14 -0400

    Update index.js

--- api/youtube/index.js
+++ api/youtube/index.js
@@ -8,12 +8,10 @@ module.exports = async (req, res) => {
       return res.status(400).json({ error: 'No URL provided' });
     }
 
-    // Make the HTTP request
+    // Make the HTTP request without Accept-Language and Accept-Encoding headers
     const response = await axios.get(url, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.5735.133 Safari/537.36',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Accept-Encoding': 'gzip, deflate, br',
       },
       // Set responseType to 'arraybuffer' to handle binary data
       responseType: 'arraybuffer',
